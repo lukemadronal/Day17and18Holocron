5e5aaf11ed15f28653196af4429ed388fd3fd3dc
Luke Madronal <madronal@lafayette.edu>
2015-10-15 12:33:48 -0400

    initial commit of iForce.

---
+++ iForce/iForce/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iForce
+//
+//  Created by Luke Madronal on 10/15/15.
+//  Copyright © 2015 Luke Madronal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
